48326b123d86e3dca104239dc40fce1df4b1077f
medve_pe <peter.medvesek@gorenje.com>
2020-02-07 09:31:44 +0100

    add comment and rename pointer variable with p prefix

--- sw_modules/data_err_check/error_check.h
+++ sw_modules/data_err_check/error_check.h
@@ -16,15 +16,23 @@
 #include <stddef.h>
 
 //============================= error_check_algorithms ============================
-#define _USE_CRC		1
-#define _USE_CHECK_SUM	2
+#define _USE_CRC        1
+#define _USE_CHECK_SUM  2
 //=================================================================================
 
 //============================ error_check_algorithm_selected =====================
-#define ERR_CHECK_METHOD	(_USE_CRC)
+#define ERR_CHECK_METHOD    (_USE_CRC)
 //=================================================================================
 
-void error_check(uint8_t* data, size_t size, uint8_t* error_check_out);
+
+/**
+ * @brief calculate error check value use algorithm specified with ERR_CHECK_METHOD
+ * 
+ * @param pData             : pointer to input data  
+ * @param size              : size of input data
+ * @param pError_check_out  : pointer to location where calculated error_check value will be put
+ */
+void error_check(const uint8_t* const  pData, size_t size, uint8_t* pError_check_out);
 
 
 #endif /* ERROR_CHECK_INCL_H_ */
